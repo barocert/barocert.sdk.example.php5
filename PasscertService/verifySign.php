111ec5abe603a3998677e7e3bdb807d6db255344
정우석 <wsj@linkhubcorp.com>
2023-08-22 13:29:56 +0900

    패스써트 검증응답정보 외국인여부 추가

--- PasscertService/verifySign.php
+++ PasscertService/verifySign.php
@@ -54,6 +54,7 @@
                 <li>수신자 출생월일 (ReceiverDay) : <?php echo $result->receiverDay ?></li>
                 <li>수신자 성별 (ReceiverGender) : <?php echo $result->receiverGender ?></li>
                 <li>수신자 휴대폰번호 (ReceiverHP) : <?php echo $result->receiverHP ?></li>
+                <li>외국인 여부 (ReceiverForeign) : <?php echo $result->receiverForeign ?></li>
                 <li>수신자 통신사유형 (ReceiverTelcoType) : <?php echo $result->receiverTelcoType ?></li>
                 <li>전자서명 데이터 (SignedData) : <?php echo $result->signedData ?></li>
                 <li>연계정보 (Ci) : <?php echo $result->ci ?></li>
